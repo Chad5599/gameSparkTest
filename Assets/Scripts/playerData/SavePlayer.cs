b7990130cd2b8848ee28ee64353a906ac6216534
“Chad5599” <irtazamumtaz10@gmail.com>
2017-09-05 19:11:48 +0500

    Add leaderboard

--- Assets/Scripts/playerData/SavePlayer.cs
+++ Assets/Scripts/playerData/SavePlayer.cs
@@ -7,10 +7,23 @@ using UnityEngine.UI;
 
 public class SavePlayer : MonoBehaviour {
 
-	public Text gold, xp,playerID,playerPos;
-	private int goldCount = 0;
+	public Text gold, xp,playerID,playerPos,score;
+	private int goldCount = 0,scoreCount = 0;
 	private int xpCount = 0;
+	private string userIDGloabl;
 
+	void Awake()
+	{
+	
+	
+			GameSparks.Api.Messages.NewHighScoreMessage.Listener += HighScoreMessageHandler;
+	}
+
+		void HighScoreMessageHandler(GameSparks.Api.Messages.NewHighScoreMessage _message) 
+	{
+			Debug.Log("NEW HIGH SCORE \n " + _message.LeaderboardName);
+
+	}
 
 	void Start()
 	{
@@ -18,12 +31,12 @@ public class SavePlayer : MonoBehaviour {
 			if (!response.HasErrors) {
 				Debug.Log("Received Player Data From GameSparks...");
 				var data = response.ScriptData.GetGSData("player_Data");
-				print("Player ID: " + data.GetString("playerID"));
-				print("Player XP: " + data.GetString("playerXP"));
-				print("Player Gold: " + data.GetString("playerGold"));
-				print("Player Pos: " + data.GetString("playerPos"));
+				//print("Player ID: " + data.GetString("playerID"));
+				//print("Player XP: " + data.GetString("playerXP"));
+				//print("Player Gold: " + data.GetString("playerGold"));
+				//print("Player Pos: " + data.GetString("playerPos"));
 
-				playerID.text = data.GetString("playerID");
+				userIDGloabl = data.GetString("playerID");
 				playerPos.text = data.GetString("playerPos");
 				xp.text = data.GetString("playerXP");
 				gold.text = data.GetString("playerGold");
@@ -37,6 +50,29 @@ public class SavePlayer : MonoBehaviour {
 			else 
 			{
 				Debug.Log("Error Loading Player Data...");
+			}
+		});
+			
+		new GameSparks.Api.Requests.LeaderboardDataRequest().SetLeaderboardShortCode("SCORE_LEADERBOARD").SetEntryCount(100).Send((response) => {
+			if (!response.HasErrors) {
+				Debug.Log("Found Leaderboard Data...");
+				foreach(GameSparks.Api.Responses.LeaderboardDataResponse._LeaderboardData entry in response.Data) {
+					int rank = (int) entry.Rank;
+					string userID = entry.UserId;
+					string playerName = entry.UserName;
+					string scoreGet = entry.JSONData["SCORE"].ToString();
+					Debug.Log("Rank:" + rank + " Name:" + playerName + " \n Score:" + score);
+
+					if(userID == userIDGloabl) 
+					{
+						playerID.text = playerName;
+						scoreCount = Convert.ToInt32(scoreGet); 
+						score.text = scoreCount.ToString();
+					}
+	
+				}
+			} else {
+				Debug.Log("Error Retrieving Leaderboard Data...");
 			}
 		});
 	}
@@ -54,6 +90,14 @@ public class SavePlayer : MonoBehaviour {
 					Debug.Log("Error Saving Player Data...");
 				}
 			} );
+
+		new GameSparks.Api.Requests.LogEventRequest().SetEventKey("SUBMIT_SCORE").SetEventAttribute("SCORE",score.text).Send((response) => {
+			if (!response.HasErrors) {
+				Debug.Log("Score Posted Successfully...");
+			} else {
+				Debug.Log("Error Posting Score...");
+			}
+		});
 	}
 
 	void OnTriggerEnter(Collider other) 
@@ -65,11 +109,15 @@ public class SavePlayer : MonoBehaviour {
 			++goldCount;
 			gold.text = goldCount.ToString ();
 
+			scoreCount = (goldCount*10);
+			score.text = scoreCount.ToString ();
+
 			if (goldCount % 5 == 0) 
 			{
 				++xpCount;
 				xp.text = xpCount.ToString ();
 			}
+				
 		}
 	}
 
